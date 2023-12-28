930e6177bb294f967545d7fae0b1826dd0c4ee02
MX RESEARCH <mx.research@mxind.com>
2023-12-28 20:06:46 +0800

    Update Configuration_MINIK3_adv.h

--- include/marlin/Configuration_MINIK3_adv.h
+++ include/marlin/Configuration_MINIK3_adv.h
@@ -1583,8 +1583,8 @@
     #endif
 
     #if AXIS_IS_TMC(E0)
-        #define E0_CURRENT 500 //520
-        #define E0_MICROSTEPS 16 //32
+        #define E0_CURRENT 550 //520
+        #define E0_MICROSTEPS 8 //32
         #define E0_RSENSE 0.22
     #endif
 
