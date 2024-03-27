878f7644fb29a18ad7b7b24bfec71b5f5c3846c3
MX RESEARCH <mx.research@mxind.com>
2024-03-28 00:12:07 +0800

    Revert "Update Configuration_MINIK3.h"
    This reverts commit b83dfade54b21c4dad61e7501ce01f22774badef.

--- include/marlin/Configuration_MINIK3.h
+++ include/marlin/Configuration_MINIK3.h
@@ -1484,9 +1484,9 @@
     #define X_AXIS_UNLOAD_POS  ((X_MAX_POS) -30)
     // Specify a park position as { X, Y, Z }
     #define NOZZLE_PARK_POINT \
-        { (X_MAX_POS - 25), (Y_MAX_POS - 10), 20 }
+        { (X_MAX_POS - 10), (Y_MAX_POS - 10), 20 }
         #define NOZZLE_PARK_POINT_M600 \
-        {(X_MAX_POS - 25), (Y_MIN_POS + 10), 20 }
+        {(X_MAX_POS - 10), (Y_MIN_POS + 10), 20 }
     #define NOZZLE_PARK_XY_FEEDRATE 100 // (mm/s) X and Y axes feedrate (also used for delta Z axis)
     #define NOZZLE_UNPARK_XY_FEEDRATE 30 // (mm/s) X and Y axes feedrate for unparking after m600
     #define NOZZLE_PARK_Z_FEEDRATE 5 // (mm/s) Z axis feedrate (not used for delta printers)
